823a8f6f5e424b50ee02ce731325ba50e2fdd5af
Olivier Chavarin <memoire1@gmail.com>
2016-10-18 08:04:28 +0200

    update

--- ScripturaForms.php
+++ ScripturaForms.php
@@ -10,118 +10,140 @@
 // @License: ISC
 // -----------------------------------------------------------------------------
 
+
+// @subsection  Languages
+// @description Externalisation du plugin
 // -----------------------------------------------------------------------------
-// @section     Shortcodes
-// @description Configuration du shortcode
+
+// @note Fichiers d'externalisation pour les traductions du thème
+// @note Name de domaine et emplacement des fichiers de traduction
+//load_theme_textdomain( 'scripturaforms', plugins_url( 'Languages', __FILE__ ) );
+load_plugin_textdomain( 'scripturaforms', false, dirname( plugin_basename( __FILE__ ) ) . '/Languages' );
+
+
+// -----------------------------------------------------------------------------
+// @section     Contact Form
+// @description Configuration du formulaire de contact
 // -----------------------------------------------------------------------------
 
 if ( ! is_admin() ) {
 
   $uri = '//' . $_SERVER[ 'HTTP_HOST' ] . $_SERVER[ 'REQUEST_URI' ]; // Alternative à get_permalink()
-
-  function ScripturaContactForm( $mail = '', $uri = '' )
+  function ScripturaContactForm ( $mail = '', $uri = '' )
   {
-
+    // BEGIN Test
+    $capacityRead = current_user_can( 'read' );
+    // END Test
     ob_start();
-
     // Passage à la ligne, normalisation pour certaines messageries
     if ( ! preg_match( '#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#', $mail ) ) {
         $oel = "\r\n";
     } else {
         $oel = "\n";
     }
-
     if ( isset( $_POST[ 'submit' ] ) ) : // Cette condition est un test permettant d'éviter un conflit avec un autre formulaire placé sur la même page
     if ( ! empty( $_POST ) ) :
     extract( $_POST ); // @note Vérification nom de variable valide
     $valid = true;
-
     // BEGIN Controllers
-
     // Controller name:
     if ( strlen( $nom ) < 2 ) {
       $valid = false;
-      $erreurNom = '<p>' . __( 'Invalid field: name contains a single character.', 'scriptura' ) . '</p>';
+      $erreurNom = '<p>' . __( 'Invalid field: your name contains a single character.', 'scripturaforms' ) . '</p>';
     }
     if ( preg_match( '#.{30,}#', $nom ) ) {
       $valid = false;
-      $erreurNom = '<p>' . __( 'Invalid field: Your name is longer than 30 characters.', 'scriptura' ) . '</p>';
+      $erreurNom = '<p>' . __( 'Invalid field: your name is longer than 30 characters.', 'scripturaforms' ) . '</p>';
     }
     if ( ! preg_match( '#^[A-Z]#', $nom ) ) {
       $valid = false;
-      $erreurNom = '<p>' . __( 'Invalid field: it lacks a capital letter at the beginning of your name.', 'scriptura' ) . '</p>';
+      $erreurNom = '<p>' . __( 'Invalid field: it lacks a capital letter at the beginning of your name.', 'scripturaforms' ) . '</p>';
     }
     if ( preg_match( '#[0-9]#', $nom ) ) {
       $valid = false;
-      $erreurNom = '<p>' . __( 'Invalid field: numeric characters are placed in your name.', 'scriptura' ) . '</p>';
+      $erreurNom = '<p>' . __( 'Invalid field: numeric characters are placed in your name.', 'scripturaforms' ) . '</p>';
     }
     if ( preg_match( '#[&!?/\+=_;:,$*()<>§@\#\".]#', $nom ) ) {
       $valid = false;
-      $erreurNom = '<p>' . __( 'Invalid field: special characters or punctuation are placed in your name.', 'scriptura' ) . '</p>';
+      $erreurNom = '<p>' . __( 'Invalid field: special characters or punctuation are placed in your name.', 'scripturaforms' ) . '</p>';
     }
     if ( empty( $nom ) ) {
       $valid = false;
-      $erreurNom = '<p>' . __( 'Invalid field: Your name is not specified.', 'scriptura' ) . '</p>';
+      $erreurNom = '<p>' . __( 'Invalid field: your name is not specified.', 'scripturaforms' ) . '</p>';
     }
-
     // Controller email:
     if ( ! preg_match( '#^[a-z0-9._-]{3,50}@[a-z0-9._-]{2,}\.[a-z]{2,4}$#i', $email ) ) {
       $valid = false;
-      $erreurEmail = '<p>' . __( 'Invalid field: Your email is not consistent.', 'scriptura' ) . '</p>';
+      $erreurEmail = '<p>' . __( 'Invalid field: your email is not consistent.', 'scripturaforms' ) . '</p>';
     }
     if ( empty( $email ) ) {
       $valid = false;
-      $erreurEmail = '<p>' . __( 'Invalid field: Your email is blank.', 'scriptura' ) . '</p>';
+      $erreurEmail = '<p>' . __( 'Invalid field: your email is blank.', 'scripturaforms' ) . '</p>';
     }
-
     // Controller tel:
     if ( ! empty( $tel ) ) {
       if ( ! preg_match( '#^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$#', $tel ) ) {
         $valid = false;
-        $erreurTel = '<p>' . __( 'Invalid field: This item does not correspond to a local or international number. Do not put any spaces, dashes or points.', 'scriptura' ) . '</p>';
+        $erreurTel = '<p>' . __( 'Invalid field: this item does not correspond to a local or international number. Do not put any spaces, dashes or points.', 'scripturaforms' ) . '</p>';
       }
     }
-
     // Controller pays:
     if ( preg_match( '#00#', $pays ) ) {
       $valid = false;
-      $erreurPays = '<p>' . __( 'You must choose a country.', 'scriptura' ) . '</p>';
+      $erreurPays = '<p>' . __( 'You must choose a country.', 'scripturaforms' ) . '</p>';
     }
-
     // Controller cover:
     if ( ! empty( $test ) ) {
       $valid = false;
     }
-
     // Controller message:
     if ( ( preg_match( '#<a(.+)<a(.+)<a#', $message ) )
     OR ( preg_match( '#http(.+)http(.+)http#', $message ) ) ) {
       $valid = false;
-      $erreurMessage = '<p>' . __( 'Invalid field : Your Message contains more than two links (spam protection).', 'scriptura' ) . '</p>';
+      $erreurMessage = '<p>' . __( 'Invalid field: your message contains more than two links (spam protection).', 'scripturaforms' ) . '</p>';
     }
     if ( strlen( $message ) < 10 ) {
       $valid = false;
-      $erreurMessage = '<p>' . __( 'Invalid field : Your Message has less than 10 characters.', 'scriptura' ) . '</p>';
+      $erreurMessage = '<p>' . __( 'Invalid field: your message has less than 10 characters.', 'scripturaforms' ) . '</p>';
     }
     if ( strlen( $message ) > 4000 ) {
       $valid = false;
-      $erreurMessage = '<p>' . __( 'Invalid field : Your message is longer than 4000 characters.', 'scriptura' ) . '</p>';
+      $erreurMessage = '<p>' . __( 'Invalid field: your message is longer than 4000 characters.', 'scripturaforms' ) . '</p>';
     }
     if ( empty( $message ) ) {
       $valid = false;
-      $erreurMessage = '<p>' . __( 'Invalid field : you have not completed your message.', 'scriptura' ) . '</p>';
+      $erreurMessage = '<p>' . __( 'Invalid field: you have not completed your message.', 'scripturaforms' ) . '</p>';
     }
+
+
+// BEGIN Test
+if ( $capacityRead ) {
+  $uploaddir = '//test/';
+  $uploadfile = $uploaddir . basename($_FILES['file']['name']);
+  echo '<div class="message">';
+  if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+      echo "Le fichier est valide, et a été téléchargé
+             avec succès. Voici plus d'informations :\n";
+  } else {
+      echo "Attaque potentielle par téléchargement de fichiers.
+            Voici plus d'informations :\n";
+  }
+  echo '</div>';
+}
+// END Test
+
+
+
+
 
     // Controller-error:
     if ( ! $valid ) {
       $erreurNotification = '<div id="popin" class="popin">';
-      $erreurNotification .= '<p class="message-warning">' . __( 'An error was detected on the form and it could not be sent. Please check all fields.', 'scriptura' ) . '</p>';
+      $erreurNotification .= '<p class="message-warning">' . __( 'An error was detected on the form and it could not be sent. Please check all fields.', 'scripturaforms' ) . '</p>';
       $erreurNotification .= '<a href="#" id="cmd-popin"></a>';
       $erreurNotification .= '</div>';
     }
-
     // END Controllers
-
     if ( $valid ) :
       $nom = htmlentities( addslashes( $nom ) );
       $nom = stripslashes( $_POST[ 'nom' ] );
@@ -129,6 +151,11 @@ if ( ! is_admin() ) {
       $tel = htmlentities( addslashes( $tel ) );
       $message = htmlentities( addslashes( $message ) );
       $message = stripslashes( $_POST[ 'message' ] );
+      // BEGIN Test
+      if ( $capacityRead ) {
+        $file = htmlentities( addslashes( $file ) );
+      }
+      // END Test
       $emailContacts = get_option( 'scriptura_emails_contact' );
     if ( $emailsContact ) {
       $to = $emailContacts;
@@ -138,82 +165,81 @@ if ( ! is_admin() ) {
       $toCopie = $email;
       $subject = 'Formulaire de contact';
       $subjectCopie = 'Copie de votre message';
-      $post  = 'Nom : ' . $nom . '' . $oel;
-      $post .= 'Email : ' . $email . '' . $oel;
-      $post .= 'Téléphone : ' . $tel . '' . $oel;
-      $post .= 'Pays : ' . $pays . '' . $oel . '' . $oel;
-      $post .= 'Message : ' . $message;
+      $post  = __( 'Name:', 'scripturaforms' ) . $nom . '' . $oel;
+      $post .= __( 'Email:', 'scripturaforms' ) . $email . '' . $oel;
+      $post .= __( 'Phone:', 'scripturaforms' ) . $tel . '' . $oel;
+      $post .= __( 'Country:', 'scripturaforms' ) . $pays . '' . $oel . '' . $oel;
+      $post .= __( 'Message:', 'scripturaforms' ) . $message;
       $header = 'Reply-To: ' . $to . '' . $oel;
       $headerCopie = 'Reply-To: ' . $email . '' . $oel;
       mail( $to, $subject, $post, $header );
       mail( $toCopie, $subjectCopie, $post, $headerCopie );
-      $formPost = '<div><p class="message-success">' . __( 'Thank you Mr (Mrs)', 'scriptura' ) . ' <b>' . $nom . '</b>. ' . __( 'Your message has been posted. A copy was sent to the entered email address', 'scriptura' ) . ' (' . $email . ').</p></div>';
-      $formPost .= '<div><form action="' . $uri . '" method="post"><button class="button"><span class="icon-paper-plane"></span>&nbsp;&nbsp;' . __( 'New message', 'scriptura' ) . '</button></form></div>';
+      $formPost = '<div><p class="message-success">' . __( 'Thank you Mr (Mrs)', 'scripturaforms' ) . ' <b>' . $nom . '</b>. ' . __( 'Your message has been posted. A copy was sent to the entered email address', 'scripturaforms' ) . ' (' . $email . ').</p></div>';
+      $formPost .= '<div><form action="' . $uri . '" method="post"><button class="button"><span class="icon-paper-plane"></span>&nbsp;&nbsp;' . __( 'New message', 'scripturaforms' ) . '</button></form></div>';
       unset( $nom );
       unset( $email );
       unset( $tel );
       unset( $pays );
       unset( $test );
       unset( $message );
+      // BEGIN Test
+      if ( $capacityRead ) {
+        unset( $file );
+      }
+      // END Test
     endif;
     endif;
     endif; // isset($_POST[ 'submit' ] )
     if ( isset( $formPost ) ) :
     echo $formPost;
       else :
-    //echo '<h2 class="emphasized-left">' . __( 'Contact form', 'scriptura' ) . '</h2>';
+    //echo '<h2 class="emphasized-left">' . __( 'Contact form', 'scripturaforms' ) . '</h2>';
     echo '<form method="post" action="' . $uri . '">';
     echo '<fieldset>';
     if ( isset( $erreurNotification ) )
       echo $erreurNotification;
-
     $invalidNom = '';
     if ( isset( $erreurNom ) )
       $invalidNom = ' invalid';
-    echo '<div class="input' . $invalidNom . '"><label for="nom">' . __( 'Name', 'scriptura' ) . '</label><input type="text" name="nom" id="nom" value="';
+    echo '<div class="input' . $invalidNom . '"><label for="nom">' . __( 'Name', 'scripturaforms' ) . '</label><input type="text" name="nom" id="nom" value="';
     if ( isset( $nom ) )
       echo stripslashes( $nom );
-    echo '" size=23 placeholder="' . __( 'John Smith', 'scriptura' ) . '" class="required" required="required">';
+    echo '" size=23 placeholder="' . __( 'John Smith', 'scripturaforms' ) . '" class="required" required="required">';
     if ( isset( $erreurNom ) )
       echo $erreurNom;
     echo '</div>';
-
     $invalidEmail = '';
     if ( isset( $erreurEmail ) )
       $invalidEmail = ' invalid';
-    echo '<div class="input' . $invalidEmail . '"><label for="email">' . __( 'Email', 'scriptura' ) . '</label><input type="email" name="email" id="email" value="';
+    echo '<div class="input' . $invalidEmail . '"><label for="email">' . __( 'Email', 'scripturaforms' ) . '</label><input type="email" name="email" id="email" value="';
     if ( isset( $email ) )
       echo $email;
     echo '" size=23 placeholder="pseudo@gmail.com" class="required" required="required">';
     if ( isset( $erreurEmail ) )
       echo $erreurEmail;
     echo '</div>';
-
     $invalidTel = '';
     if ( isset( $erreurTel ) )
       $invalidTel = ' invalid';
-    echo '<div class="input' . $invalidTel . '"><label for="tel">' . __( 'Phone', 'scriptura' ) . '</label><input type="tel" name="tel" id="tel" value="';
+    echo '<div class="input' . $invalidTel . '"><label for="tel">' . __( 'Phone', 'scripturaforms' ) . '</label><input type="tel" name="tel" id="tel" value="';
     if ( isset( $tel ) )
       echo stripslashes( $tel );
     echo '" size=23  placeholder="0158808080">';
     if ( isset( $erreurTel ) )
       echo $erreurTel;
     echo '</div>';
-
     $invalidPays = '';
     if ( isset( $erreurPays ) )
       $invalidPays = ' invalid';
-    echo '<div class="input' . $invalidPays . '"><label for="pays"><span class="icon-world"></span><span>' . __( 'Country', 'scriptura' ) . '</span></label>';
+    echo '<div class="input' . $invalidPays . '"><label for="pays"><span class="icon-world"></span><span>' . __( 'Country', 'scripturaforms' ) . '</span></label>';
     echo '<select name="pays" id="pays" value="';
     if ( isset( $pays ) )
       echo $pays;
     echo '" class="required">';
-    echo '<option value="00" selected="selected">' . __( 'Country', 'scriptura' ) . '</option>'; // Sélection par défaut
+    echo '<option value="00" selected="selected">' . __( 'Country', 'scripturaforms' ) . '</option>'; // Sélection par défaut
     echo '<option value="FR">France</option>'; // Sélection mise en avant
-
     $uriCsv = plugin_dir_path( __FILE__ ) . 'ListOfCountries.csv'; // Liste des pays, ISO 3166-1
     //@note Le chemin doit être définit localement afin d'être compatible avec le SSL.
-
     // Boucle récupérant la liste de tous les pays
     $id_file = fopen( $uriCsv, 'r' ); // 'r' lecture seule
     while ( $line = fgets( $id_file, 1024 ) ) { // '1024' Nombre d'octets max par ligne et par défaut
@@ -222,32 +248,42 @@ if ( ! is_admin() ) {
       if ( $pays == $line[0] ) // Sélection en cours
         echo '<option value="' . $line[ 0 ] . '" selected="selected">' . $line[ 1 ] . '</option>'; // Retourne la sélection en cours
     }
-
     echo '</select>';
     if ( isset( $erreurPays ) )
       echo $erreurPays;
     echo '</div>';
-
     echo '<div class="input hidden"><label for="test">Test</label>';
     echo '<input type="text" name="test" id="test" value="';
     if ( isset( $test ) )
       echo stripslashes( $test );
     echo '" size=23 placeholder="requis"></div>';
-
     $invalidMessage = '';
     if ( isset( $erreurMessage ) )
       $invalidMessage = ' invalid';
-    echo '<div class="input' . $invalidMessage . '"><label for="message">' . __( 'Your message', 'scriptura' ) . '</label><textarea name="message" id="message" placeholder="' . __( 'Good Morning...', 'scriptura' ) . '" class="required" required="required">';
+    echo '<div class="input' . $invalidMessage . '"><label for="message">' . __( 'Your message', 'scripturaforms' ) . '</label><textarea name="message" id="message" placeholder="' . __( 'Good Morning...', 'scripturaforms' ) . '" class="required" required="required">';
     if ( isset( $message ) )
       echo stripslashes( $message );
     echo '</textarea>';
     if ( isset( $erreurMessage ) )
       echo $erreurMessage;
     echo '</div>';
-    echo '<div><button class="button" name="submit"><span class="icon-paper-plane"></span>&nbsp;&nbsp;' . __('Submit', 'scriptura') . '</button></div>';
+
+    // BEGIN Test
+    if ( $capacityRead ) {
+        echo '<div class="input"><label for="file">' . __( 'File', 'scripturaforms' ) . '</label><input type="file" name="file" id="file" value="';
+      if ( isset( $file ) )
+        echo stripslashes( $file );
+      echo '" size=23>';
+      if ( isset( $erreurFile ) )
+        echo $erreurFile;
+      echo '</div>';
+    }
+    // END Test
+
+    echo '<div><button class="button" name="submit"><span class="icon-paper-plane"></span>&nbsp;&nbsp;' . __('Submit', 'scripturaforms') . '</button></div>';
     echo '</fieldset>';
     echo '</form>';
-    endif; // isset($formPost)
+    endif; // isset( $formPost )
     $contactForm = ob_get_clean();
     return $contactForm;
   }
@@ -285,5 +321,5 @@ if ( is_admin() ) {
       return $buttons;
     }
 
-}
+} // admin
 
